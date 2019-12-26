499774cf5197a0f1f41ab805e68cedab2cb8feee
oieioi <atsuinatsu.samuifuyu@gmail.com>
2019-12-26 15:11:28 +0900

    aaa

--- lib/field_reader.rb
+++ lib/field_reader.rb
@@ -2,11 +2,26 @@ require_relative './tiles'
 
 module City
   class FieldReader
-    def self.read(file_path)
+    def self.read_tiles(file_path)
       result = nil
 
       File.open(file_path) do |file|
         result = file.each_line.map do |line|
+          break if line == 'END'
+          line.each_char.map { |char| make_tile(char)}
+        end
+      end
+
+      result
+    end
+
+    def self.read_movables(file_path)
+      result = nil
+
+      File.open(file_path) do |file|
+        result = file.each_line.map do |line|
+          break if line == 'TILES_END'
+          break if line == 'TILES_END'
           line.each_char.map { |char| make_tile(char)}
         end
       end
